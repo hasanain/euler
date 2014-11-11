b50fc79601b5e4d48a304806d882ad6f2e8d5cd1
Hasanain Jamal <hasanain.jm@gmail.com>
2014-11-10 20:38:35 -0700

    Problem 58 now takes 0.4 of a second rather than 30mins

--- Euler58.java
+++ Euler58.java
@@ -6,35 +6,34 @@ public class Euler58 {
 		Date start, end;
 		start = new Date();
 
-		int sideLength =3;
-		double ratio = spiralPrimeDiagonals(sideLength);
-		while ( ratio > 0.1d){
-			sideLength +=2;	
-			ratio = spiralPrimeDiagonals(sideLength);
-			System.out.println(sideLength + " : " + ratio);
-		}
-		System.out.println(sideLength);
+		int sideLength = spiralP(0.1);
 		end = new Date();
+		System.out.println("Side Length: " + sideLength);
 		System.out.println("Execution time: "
 				+ (end.getTime() - start.getTime()));
 	}
-	private static double spiralPrimeDiagonals(int sideLength) {
-		int diagonalLength = sideLength + sideLength -1;
+
+	private static int spiralP(double ratio) {
+		int sideLength = 3;
 		double primeCounter = 0d;
 		
-		int n = sideLength -1; 
+		int maxNumberToAddToDiagonals = sideLength - 1;
 		int m = 2;
 		int l = 1;
-		for (int i = 0; m <= n; i++) {
-			if(isPrime(l)){
+		for (int i = 0; m <= maxNumberToAddToDiagonals; i++) {
+			if (isPrime(l)) {
 				primeCounter++;
 			}
 			m = (i % 4 == 0 && i != 0) ? m + 2 : m;
 			l += m;
+			if (primeCounter / ((double) 2 * maxNumberToAddToDiagonals + 1) > ratio) {
+				maxNumberToAddToDiagonals++;
+			}
 		}
-		return primeCounter/(double)diagonalLength;
+		return maxNumberToAddToDiagonals + 1;
 
 	}
+
 	public static boolean isPrime(long num) {
 
 		if (num == 0 || num == 1)
