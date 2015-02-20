644a2d9376fbe48b7077bd82cb8cb2fe0db240e9
Hasanain Jamal <hjamal@email.arizona.edu>
2015-02-19 22:47:41 -0700

    Added solution to problem 97 in js

---
+++ js/euler097.js
@@ -0,0 +1,24 @@
+/*
+Project Euler Problem 97
+========================
+
+The first known prime found to exceed one million digits was discovered in
+1999, and is a Mersenne prime of the form 2^69725931; it contains exactly
+2,098,960 digits. Subsequently other Mersenne primes, of the form 2^p1,
+have been found which contain more digits.
+
+However, in 2004 there was found a massive non-Mersenne prime which
+contains 2,357,207 digits: 28433 * 2^7830457+1.
+
+Find the last ten digits of this prime number.
+*/
+var i , j;
+i = 28433;
+
+for(j = 1; j <= 7830457; j++){
+	i*=2;
+	i%=10000000000;
+}
+i++;
+console.log(i);
+
