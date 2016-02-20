6eef7af2fe768c739fa6f0d8438f8de171c656a1
Hasanain Jamal <hasanain.jm@gmail.com>
2016-02-20 11:22:40 -0700

    Modify solution to problem 1 in python

--- python/euler001.py
+++ python/euler001.py
@@ -9,9 +9,4 @@
  Find the sum of all the multiples of 3 or 5 below 1000.
  """
 
-sum = 23
-for i in range(10, 1000):
-    if i % 5 == 0 or i % 3 == 0 :
-        sum += i
-
-print sum
+print(sum(filter(lambda x: x % 3 == 0 or x % 5 == 0, range(1000))))
