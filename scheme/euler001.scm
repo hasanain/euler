d598e306ba32baf01d56a61cea57d8a451d29a22
Hasanain Jamal <hasanain.jm@gmail.com>
2016-04-05 20:23:43 -0700

    problems solved

--- scheme/euler001.scm
+++ scheme/euler001.scm
@@ -10,25 +10,28 @@
 ; Find the sum of all the multiples of 3 or 5 below 1000.
 
 (define (sum l)
-  (cond
-    ((null? l) 0)
-    (else (+ (car l) (sum (cdr l))))))
-
+  (define (inner s l)
+    (cond
+    ((null? l) s)
+    (else (inner (+ s (car l)) (cdr l)))))
+  (inner 0 l))
 
 (define (threeOrFive? x)
   (or (= (remainder x 3) 0) (= (remainder x 5) 0)))
 
 (define (range n)
-  (define (r m)
+  (define (inner c l)
     (cond
-      ((= m (- n 1)) (list m))
-      (else (cons m (r (+ m 1))))))
-  (r 0))
+      ((< c 0) l)
+      (else (inner (- c 1) (cons c l)))))
+  (inner (- n 1) '()))
 
 (define (filter f l)
-  (cond
-    ((null? l) '())
-    ((f (car l)) (cons (car l) (filter f (cdr l))))
-    (else (filter f (cdr l)))))
-    
-(sum (filter threeOrFive? (range 1000)))
+  (define (inner f l a)
+    (cond
+      ((null? l) a)
+      ((f (car l)) (inner f (cdr l) (cons (car l) a)))
+      (else (inner f (cdr l) a))))
+  (inner f l '()))
+   
+(sum (filter2 threeOrFive? (range 1000)))
--- scheme/euler003.scm
+++ scheme/euler003.scm
@@ -1,3 +1,4 @@
+#lang scheme
 
 ; Project Euler Problem 3
 ; =======================
@@ -6,3 +7,27 @@
 ;
 ; What is the largest prime factor of the number 600851475143?
 
+(define (gcd a b)
+  (cond
+    ((= b 0) a )
+    (else (gcd b (modulo a b)))))
+
+(define (largest-prime-factor n)
+  (define (g x) (modulo (+ (* x x) 1) n))
+  (define (inner x y d)
+    (cond
+      ((<= d 1) (inner (g x) (g (g y)) (gcd (abs (- x y)) n)))
+      (else d)))
+  (inner 2 2 1))
+
+(largest-prime-factor 600851475143)
+
+(define (e3 n)
+  (define (e3-inner num div)
+    (if (> num 1)
+        (if (= (modulo num div) 0)
+            (e3-inner (/ num div) (- div 1))
+            (e3-inner num (+ div 1)))
+        (+ div 1)))
+  (e3-inner n 2))
+(e3 600851475143)
